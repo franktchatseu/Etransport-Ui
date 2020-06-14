62293c49c5795e151e079de6e11d9541ea5e0b74
VisibilityCamSarl <vcam@vmi338592.contaboserver.net>
2020-06-14 10:08:57 +0200

    ng update --next @angular/cli --force

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,12 +6,12 @@ const routes: Routes = [
   {
     path: 'private',
     loadChildren:
-      './private/private.module#PrivateModule',
+      () => import('./private/private.module').then(m => m.PrivateModule),
   },
   {
     path: 'public',
     loadChildren:
-      './public/public.module#PublicModule',
+      () => import('./public/public.module').then(m => m.PublicModule),
   }
 ];
 @NgModule({
