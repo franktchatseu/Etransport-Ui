175ec90cedc1eb38659d1578e8c4d918c9c336cc
adamshan <adamualiyu199@gmail.com>
2020-06-23 21:32:25 +0100

    'creating component of sacrament'

---
+++ src/app/services/sacrament/sacrament.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { config } from '../../config';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SacramentService {
+
+  constructor(private http: HttpClient) {}
+
+  async post(formData) {
+
+    return await this.http.post(`${config.apiUrl}/suggestions`, formData, {
+      reportProgress: true,
+      observe: 'events'
+    }).pipe(map(data => data));
+  }
+
+  public async put(id: number, formData): Promise<any> {
+    return await this.http
+        .put(`${config.apiUrl}/suggestions/${id}`, formData)
+        .pipe(map(data => data));
+  }
+
+  public get() {
+    return this.http.get<any>(`${config.apiUrl}/suggestions/page`)
+      .pipe(map(data => data));
+  }
+
+  public async delete(id) {
+    return await this.http
+        .delete(`${config.apiUrl}/suggestions/${id}`)
+        .pipe(map(data => data));
+  }
+
+}
