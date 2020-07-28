6f024596d29e16b8facd23f08a3bafc52a53ae9c
frank tchatseu <louenkamfrank@gmail.com>
2020-07-28 05:13:30 +0100

    save

--- src/app/private/containers/superadmin/services/driver.service.ts
+++ src/app/private/containers/superadmin/services/driver.service.ts
@@ -4,25 +4,37 @@ import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 
 @Injectable({
-    providedIn: 'root'
-  })
-  export class DriverService {
+  providedIn: 'root'
+})
+export class DriverService {
 
-    constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-    public getDrivers() {
-        return this.http.get<any>(`${config.apiUrl}/module2/general_informations/`)
-          .pipe(map(data => data));
-      }
-
-    public getPage(url) {
-        return this.http.get<any>(`${url}`)
-          .pipe(map(data => data));
-      }
-    
-      delete(id: number) {
-        return this.http.delete(`${config.apiUrl}/module2/general_informations/${id}`).toPromise();
-    
-      }
-    
+  public getDrivers() {
+    return this.http.get<any>(`${config.apiUrl}/module2/general_informations/`)
+      .pipe(map(data => data));
   }
+
+  public getPage(url) {
+    return this.http.get<any>(`${url}`)
+      .pipe(map(data => data));
+  }
+
+  delete(id: number) {
+    return this.http.delete(`${config.apiUrl}/module2/general_informations/${id}`).toPromise();
+
+  }
+  add(formData: FormData): Promise<any> {
+    return this.http.post<any>(`${config.apiUrl}/module2/general_informations`, formData).toPromise();
+  }
+  addPiece(formData: FormData): Promise<any> {
+    return this.http.post<any>(`${config.apiUrl}/module2/doc_identity_information`, formData).toPromise();
+  }
+  addPermis(formData: FormData): Promise<any> {
+    return this.http.post<any>(`${config.apiUrl}/module2/drivingpermits`, formData).toPromise();
+  }
+  addFormations(formData: FormData): Promise<any> {
+    return this.http.post<any>(`${config.apiUrl}/module2/formations`, formData).toPromise();
+  }
+
+}
