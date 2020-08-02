f1bbcd0cf713039b6fed0c645c5af1faeed29ba7
adamshan <adamualiyu199@gmail.com>
2020-08-02 02:50:32 +0100

    WIP

--- src/app/private/containers/superadmin/services/engin.service.ts
+++ src/app/private/containers/superadmin/services/engin.service.ts
@@ -78,5 +78,27 @@ export class EnginService {
     }
     addCarpapers(formData: FormData): Promise<any> {
       return this.http.post<any>(`${config.apiUrl}/module3/carpapers`, formData).toPromise();
-    }c
+    }
+
+    public getCarosseries() {
+      return this.http.get<any>(`${config.apiUrl}/module3/carosseries`)
+        .toPromise();
+    }
+
+    public getTypes() {
+      return this.http.get<any>(`${config.apiUrl}/module3/types`)
+        .toPromise();
+    }
+
+    public getMarks() {
+      return this.http.get<any>(`${config.apiUrl}/module3/marks`)
+        .toPromise();
+    }
+
+    public getModels() {
+      return this.http.get<any>(`${config.apiUrl}/module3/modeles`)
+        .toPromise();
+    }
+
+    
 }
