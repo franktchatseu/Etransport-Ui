67e73573f37d845ddcb99a16ba4143c4399b666f
frank tchatseu <louenkamfrank@gmail.com>
2020-08-04 22:12:56 +0100

    save das

--- src/app/private/containers/superadmin/containers/acceuil/acceuil.component.ts
+++ src/app/private/containers/superadmin/containers/acceuil/acceuil.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DashboardService} from '../../services/dashboard.service';
 
 @Component({
   selector: 'app-acceuil',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AcceuilComponent implements OnInit {
 
-  constructor() { }
+  tot_transporteur;
+  tot_driver;
+  tot_car;
+  tot_element;
+  constructor(
+    private DashboardService: DashboardService
+  ) { }
 
   ngOnInit(): void {
+    //recuperation
+    this.DashboardService.getDonne().then(
+      (Response) => {
+        console.log(Response)
+        this.tot_transporteur=Response.total_transporteur.total_transporteur;
+        this.tot_driver=Response.total_chauffeur.tatal_chauffeur;
+        this.tot_car=Response.total_engin.total_engin;
+        this.tot_element=Response.total_element.total_element;
+        
+
+      },
+      (error) => {
+        console.log(error)
+      })
   }
 
 }
